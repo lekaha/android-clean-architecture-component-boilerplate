7710c0572a0d665edd4ade1b12265e6e3561f883
lekaha <lekaha@gmail.com>
2018-01-25 00:47:42 +0900

    add dependencies of Android architecture component

---
+++ mobile-ui/src/main/java/com/lekaha/android/boilerplate/ui/browse/BrowseFragment.kt
@@ -0,0 +1,19 @@
+package com.lekaha.android.boilerplate.ui.browse
+
+import com.lekaha.android.boilerplate.ui.BaseInjectingFragment
+
+class BrowseFragment: BaseInjectingFragment() {
+
+    companion object {
+        fun newInstance(): BrowseFragment = BrowseFragment()
+    }
+
+    override fun onInject() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getLayoutId(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
