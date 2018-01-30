551ee4f6b457f61748fc2837e1cc891383e1b78b
lekaha <lekaha@gmail.com>
2018-01-30 12:25:51 +0900

    feat: add instruments for helping development
    - Stetho, LeakCanary, and Chuck
    - Set the minimun sdk to 16

--- remote/src/main/java/com/lekaha/android/boilerplate/remote/BufferooServiceFactory.kt
+++ remote/src/main/java/com/lekaha/android/boilerplate/remote/BufferooServiceFactory.kt
@@ -3,6 +3,7 @@ package com.lekaha.android.boilerplate.remote
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -16,9 +17,8 @@ import java.util.concurrent.TimeUnit
  */
 object BufferooServiceFactory {
 
-    fun makeBuffeoorService(isDebug: Boolean): BufferooService {
-        val okHttpClient = makeOkHttpClient(
-                makeLoggingInterceptor(isDebug))
+    fun makeBuffeoorService(isDebug: Boolean, vararg interceptors: Interceptor): BufferooService {
+        val okHttpClient = makeOkHttpClient(isDebug, *interceptors)
         return makeBufferooService(okHttpClient, makeGson())
     }
 
@@ -35,6 +35,16 @@ object BufferooServiceFactory {
     private fun makeOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
         return OkHttpClient.Builder()
                 .addInterceptor(httpLoggingInterceptor)
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .build()
+    }
+
+    private fun makeOkHttpClient(isDebug: Boolean, vararg interceptors: Interceptor): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        builder.addInterceptor(makeLoggingInterceptor(isDebug))
+        if (isDebug) interceptors.forEach { builder.addInterceptor(it) }
+        return builder
                 .connectTimeout(120, TimeUnit.SECONDS)
                 .readTimeout(120, TimeUnit.SECONDS)
                 .build()
