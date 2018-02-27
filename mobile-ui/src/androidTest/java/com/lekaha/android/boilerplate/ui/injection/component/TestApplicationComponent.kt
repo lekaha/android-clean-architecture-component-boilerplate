d7a6e09d8a05c2df468dcb36b0bd00d3c15b51b5
lekaha <lekaha@gmail.com>
2018-02-27 11:09:03 +0900

    fix: build failed in mobile-ui::AndroidTest

--- mobile-ui/src/androidTest/java/com/lekaha/android/boilerplate/ui/injection/component/TestApplicationComponent.kt
+++ mobile-ui/src/androidTest/java/com/lekaha/android/boilerplate/ui/injection/component/TestApplicationComponent.kt
@@ -1,18 +1,20 @@
 package com.lekaha.android.boilerplate.ui.injection.component
 
 import android.app.Application
-import dagger.BindsInstance
-import dagger.Component
-import dagger.android.support.AndroidSupportInjectionModule
 import com.lekaha.android.boilerplate.domain.executor.PostExecutionThread
 import com.lekaha.android.boilerplate.domain.repository.BufferooRepository
-import com.lekaha.android.boilerplate.ui.injection.module.ActivityBindingModule
+import com.lekaha.android.boilerplate.ui.injection.module.TestActivityBindingModule
 import com.lekaha.android.boilerplate.ui.injection.module.TestApplicationModule
 import com.lekaha.android.boilerplate.ui.injection.scopes.PerApplication
 import com.lekaha.android.boilerplate.ui.test.TestApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.support.AndroidSupportInjectionModule
 
-@Component(modules = arrayOf(TestApplicationModule::class, ActivityBindingModule::class,
-        AndroidSupportInjectionModule::class))
+@Component(
+    modules = [TestApplicationModule::class, TestActivityBindingModule::class,
+        AndroidSupportInjectionModule::class]
+)
 @PerApplication
 interface TestApplicationComponent : ApplicationComponent {
 
